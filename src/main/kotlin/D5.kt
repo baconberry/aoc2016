453a6ae52efd6276066821e5ffb7cf6e0d750d27
Heli Villarreal <heli.v@pm.me>
2025-04-21 23:18:40 +0200

    Solved day 5 part 2

--- src/main/kotlin/D5.kt
+++ src/main/kotlin/D5.kt
@@ -4,19 +4,41 @@ class D5 : Solver {
     override fun solve(lines: Array<String>, part: UInt): String {
         val word = lines.first()
         val password = StringBuilder()
+        val passwordArr = MutableList(8) { "" }
         val md = MessageDigest.getInstance("MD5")
         var i = 0
         while (true) {
             val md5Val = "$word$i".digest(md)
             if (md5Val.headingZeroes() >= 5u) {
-                password.append(md5Val[5])
-                if (password.length == 8) {
-                    break
+                if (part == 2u) {
+                    if (part2Password(md5Val, passwordArr, password)) break
+                } else {
+                    password.append(md5Val[5])
+                    if (password.length == 8) {
+                        break
+                    }
                 }
             }
             i++
         }
         return password.toString()
+    }
+
+    private fun part2Password(
+        md5Val: String,
+        passwordArr: MutableList<String>,
+        password: StringBuilder
+    ): Boolean {
+        val pos = md5Val[5].digitToInt(16)
+        val char = md5Val[6]
+        if (pos in 0..7 && passwordArr[pos] == "") {
+            passwordArr[pos] = "$char"
+        }
+        if (passwordArr.notBlankCount() == 8) {
+            password.append(passwordArr.joinToString(""))
+            return true
+        }
+        return false
     }
 }
 
@@ -35,4 +57,8 @@ fun String.headingZeroes(): UInt {
         count++
     }
     return count
+}
+
+fun List<String>.notBlankCount(): Int {
+    return this.count { it.isNotBlank() }
 }
--- src/test/kotlin/D5Test.kt
+++ src/test/kotlin/D5Test.kt
@@ -1,10 +1,11 @@
-import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import kotlin.test.Ignore
 
 class D5Test {
 
     @Test
+    @Ignore("Slow test")
     fun solve() {
         val solver = D5()
         val inputList = listOf(
@@ -12,6 +13,18 @@ class D5Test {
         )
         for ((input, result) in inputList) {
             assertEquals(result, solver.solve(input.split("\n").toTypedArray(), 1u))
+        }
+    }
+
+    @Test
+    @Ignore("Slow test")
+    fun solve_Part2() {
+        val solver = D5()
+        val inputList = listOf(
+            "abc" to "05ace8e3",
+        )
+        for ((input, result) in inputList) {
+            assertEquals(result, solver.solve(input.split("\n").toTypedArray(), 2u))
         }
     }
 }
